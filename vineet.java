4aa8c8536f2f0741d07bc0802d25778fa7e07913
vineet950 <vineetkumar9006@gmail.com>
2022-07-15 13:22:44 +0530

    commit

---
+++ vineet.java
@@ -0,0 +1,12 @@
+change in commit
+public class vinoo {
+    public static void main(String[] args) {
+
+        int product = 1;
+
+        for (int num = 1; num <= 5; num++) {
+            product = product*num;
+        }
+        System.out.println(product);
+    }
+}
