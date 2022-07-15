3098d3fdd316e18b677c30fecea65b2180ba8c8b
vineet950 <vineetkumar9006@gmail.com>
2022-07-15 12:12:30 +0530

    first

---
+++ vinoo.java
@@ -0,0 +1,11 @@
+public class vinoo {
+    public static void main(String[] args) {
+
+        int product = 1;
+
+        for (int num = 1; num <= 5; num++) {
+            product = product*num;
+        }
+        System.out.println(product);
+    }
+}
